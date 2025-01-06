77ccccc31e2a1e5ed62925ddd0cab43097bdd0cc
Devansh <devopscsbs@gmail.com>
2025-01-06 12:41:53 +0530

    remainder added

--- app/app.py
+++ app/app.py
@@ -4,9 +4,10 @@ sum_result = a + b
 diff = b - a
 mul = a * b
 div = a / b
-
+rem = a % b
 
 print("Sum = " + str(sum_result))
 print("\nDifference = " + str(diff))
 print("\nMultiplication = " + str(mul))
-print("\nDivision = " + str(div) + "\n")
+print("\nDivision = " + str(div) )
+print ("\n Remainder = " + str(rem) + "\n")
