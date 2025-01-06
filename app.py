6fdfc8ca4f5c19fa9a26b198b1be6bd68f6433a4
Devansh <devopscsbs@gmail.com>
2025-01-06 12:27:03 +0530

    code written

---
+++ app.py
@@ -0,0 +1,12 @@
+a = 2
+b = 3
+sum_result = a + b
+diff = b - a
+mul = a * b
+div = a / b
+
+
+print("Sum = " + str(sum_result))
+print("\nDifference = " + str(diff))
+print("\nMultiplication = " + str(mul))
+print("\nDivision = " + str(div))
